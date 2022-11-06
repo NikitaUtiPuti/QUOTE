fe279850079198b78ca488693020b8e175561add
Nikita <ymnooi1998@yandex.ru>
2022-11-06 13:57:22 +0300

    Initial Commit

---
+++ QUOTE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QUOTE
+//
+//  Created by Никита Зюзин on 06.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
